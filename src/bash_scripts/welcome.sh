7aa7940f3c1684a61d69289a5dcbd92749eb85b1
Mehradi <mehradi.info@gmail.com>
2023-10-15 16:14:58 +0200

    Fixing build script with conditional parameters

--- src/bash_scripts/welcome.sh
+++ src/bash_scripts/welcome.sh
@@ -3,7 +3,13 @@
 FIRST_NAME=$1
 LAST_NAME=$2
 SHOW=$3
-if ["$SHOW"="true"]; then
+
+if [ $SHOW == 1 ] 
+then
     echo "Hello, $FIRST_NAME $LAST_NAME"
+elif [ $SHOW == 2 ]
+then
+    echo "Hello, Dear"
 else
     echo "If you want to see name, please choose the SHOW"
+fi    
